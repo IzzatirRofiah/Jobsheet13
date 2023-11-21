194da872874459bb3009630345dc6833f768dc83
IzzatirRofiah <144086912+IzzatirRofiah@users.noreply.github.com>
2023-11-21 15:07:07 +0700

    Add files via upload

---
+++ TerimaKasih_16_1.java
@@ -0,0 +1,8 @@
+package Jobsheet13;
+
+public class TerimaKasih_16_1 {
+    public static void main(String[] args) {
+        System.out.println("Thank you for being the best teacher in the world.\n" + 
+        "You inspired in me a love for learning and made me fell like I could ask yaou anything ");
+    }
+}
